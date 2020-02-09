dca95715da04b33bc75c186925d072dab5359dc2
Toni Immonen <toni.immonen0@gmail.com>
2020-02-09 17:02:10 +0200

    1.7 unicafe, step2

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -19,6 +19,9 @@ const App = () => {
         good {good}<br />
         neutral {neutral}<br />
         bad {bad}<br />
+        all {good + neutral + bad}<br />
+        average {(good - bad) / (good + neutral + bad)}<br />
+        positive {good / (good + neutral + bad)} %
       </div>
     </div>
   );
