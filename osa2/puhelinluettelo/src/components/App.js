b0d7d12ddc21e0cffb7de9c84a5dcc133adb6939
Toni Immonen <toni.immonen0@gmail.com>
2020-03-22 08:51:44 +0200

    2.9 puhelinluettelo, step4

--- osa2/puhelinluettelo/src/components/App.js
+++ osa2/puhelinluettelo/src/components/App.js
@@ -4,6 +4,7 @@ const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filterName, setfilterName] = useState('');
 
   const addPerson = event => {
     event.preventDefault();
@@ -24,18 +25,34 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const filteredPersons = persons.filter(({ name }) => name.startsWith(filterName));
+
+  const handleFilterChange = event => {
+    setfilterName(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>Name: <input value={newName} onChange={handleNameInput} /></div>
-        <div>Number: <input value={newNumber} onChange={handleNumberInput} /></div>
-        <div><button type="submit">Add</button></div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map(({ name, number }) => <li key={name}>{name} {number}</li>)}
-      </ul>
+      <div>
+        Filter shown with <input value={filterName} onChange={handleFilterChange} />
+      </div>
+      <div>
+        <h2>Add a new</h2>
+        <form onSubmit={addPerson}>
+          <div>Name: <input value={newName} onChange={handleNameInput} /></div>
+          <div>Number: <input value={newNumber} onChange={handleNumberInput} /></div>
+          <div><button type="submit">Add</button></div>
+        </form>
+      </div>
+      <div>
+        <h2>Numbers</h2>
+        <ul>
+          {filteredPersons.map(({ name, number }) =>
+            <li key={name}>{name} {number}</li>
+          )}
+        </ul>
+      </div>
     </div>
   );
 };
